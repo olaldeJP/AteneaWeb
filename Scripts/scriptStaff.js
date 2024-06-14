3b233eaea82ba1ee7f8edb13d8445d390cbd836e
olaldeJP <olaldejp@gmail.com>
2024-06-14 13:28:04 -0300

    updateCards

---
+++ Scripts/scriptStaff.js
@@ -0,0 +1,10 @@
+const cardsStaff = document.querySelector(".carStaff");
+/*
+document.addEventListener("DOMContentLoaded", function () {
+  let getImg = "1.jpg";
+
+  cardsStaff.style.backgroundImage = `url(../img/1.jpg)`;
+  cardsStaff.style.backgroundSize = "cover";
+  cardsStaff.style.backgroundPosition = "center";
+});
+*/
