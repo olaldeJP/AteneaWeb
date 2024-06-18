ec7b57b3f59ffc627261cd5a274153ca9bbeeddf
olaldeJP <olaldejp@gmail.com>
2024-06-17 23:43:14 -0300

    updateSvg

--- Scripts/scriptStaff.js
+++ Scripts/scriptStaff.js
@@ -1,3 +1,7 @@
+const slideDerecho = document.querySelector("#botonSlideDerecha");
+const slideIzquierda = document.querySelector("#botonSlideIzquierda");
+const slider = document.querySelector(".slider");
+const sliderTrack = document.querySelector(".slide-track");
 const cardsStaff = document.querySelector(".carStaff");
 /*
 document.addEventListener("DOMContentLoaded", function () {
@@ -8,3 +12,25 @@ document.addEventListener("DOMContentLoaded", function () {
   cardsStaff.style.backgroundPosition = "center";
 });
 */
+
+// Manejo de eventos para velocidad rápida
+slideDerecho.addEventListener("mouseover", () => {
+  slider.classList.add("sliderFast");
+  sliderTrack.classList.add("sliderFast");
+});
+
+slideDerecho.addEventListener("mouseout", () => {
+  slider.classList.remove("sliderFast");
+  sliderTrack.classList.remove("sliderFast");
+});
+
+// Manejo de eventos para dirección inversa
+slideIzquierda.addEventListener("mouseover", () => {
+  slider.classList.add("sliderIzquierda");
+  sliderTrack.classList.add("sliderIzquierda");
+});
+
+slideIzquierda.addEventListener("mouseout", () => {
+  slider.classList.remove("sliderIzquierda");
+  sliderTrack.classList.remove("sliderIzquierda");
+});
