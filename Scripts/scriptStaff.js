c03d4cbe5da73bc2424dabf7a4d184d551b95b44
olaldeJP <olaldejp@gmail.com>
2024-07-01 13:12:11 -0300

    update

--- Scripts/scriptStaff.js
+++ Scripts/scriptStaff.js
@@ -1,1 +1,16 @@
+const botIzq = document.querySelector("#botonIzqStaff");
+const botDer = document.querySelector("#botonDerStaff");
+const sliderTrack = document.querySelector(".slide-track");
+botIzq.addEventListener("mouseover", () => {
+  sliderTrack.classList.add("moveFastLeft");
+});
+botDer.addEventListener("mouseover", () => {
+  sliderTrack.classList.add("moveFastRight");
+});
 
+botIzq.addEventListener("mouseout", () => {
+  sliderTrack.classList.remove("moveFastLeft");
+});
+botDer.addEventListener("mouseout", () => {
+  sliderTrack.classList.remove("moveFastRight");
+});
